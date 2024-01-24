608a4bedceccf10ad63da1fe3f20bc80a10c25cc
Jakub Chojna <jakub.chojna@gmail.com>
2024-01-24 22:06:55 +0100

    feat: add simple scene

--- src/globe.js
+++ src/globe.js
@@ -1,21 +1,40 @@
-import {
-  Color,
-  LineBasicMaterial,
-  LineSegments,
-  Mesh,
-  MeshPhongMaterial,
-  SphereBufferGeometry,
-  TextureLoader
-} from 'three';
+import * as THREE from 'three';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 
-const THREE = window.THREE
-  ? window.THREE
-  : {
-    Color,
-    LineBasicMaterial,
-    LineSegments,
-    Mesh,
-    MeshPhongMaterial,
-    SphereBufferGeometry,
-    TextureLoader
-  };
+// Scene
+const scene = new THREE.Scene();
+const canvas = document.querySelector('canvas.webglobe');
+
+// Sizes
+const sizes = {
+  width: 1200,
+  height: 800
+}
+const aspectRatio = sizes.width / sizes.height;
+
+// Object
+const geometry = new THREE.BoxGeometry(1, 1, 1);
+const material = new THREE.MeshBasicMaterial({ color: '#00f' });
+const mesh = new THREE.Mesh(geometry, material);
+scene.add(mesh);
+
+// Camera
+const camera = new THREE.PerspectiveCamera(55, aspectRatio, 0.1, 100);
+camera.position.z = 3;
+scene.add(camera);
+
+const controls = new OrbitControls(camera, canvas);
+controls.enableDamping = true;
+
+const renderer = new THREE.WebGLRenderer({
+  canvas
+});
+renderer.setSize(sizes.width, sizes.height);
+renderer.render(scene, camera);
+
+const tick = () => {
+  renderer.render(scene, camera);
+  controls.update();
+  window.requestAnimationFrame(tick);
+}
+tick();
