11e7e82fce39b8f6883de8c4cf39fdfc0929cd5d
Jakub Chojna <jakub.chojna@gmail.com>
2024-01-29 22:19:26 +0100

    feat: add dots on globe surface

--- src/globe.js
+++ src/globe.js
@@ -4,11 +4,13 @@ import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 import { GeoJsonGeometry } from 'three-geojson-geometry';
 import { geoGraticule10 } from 'd3-geo';
 import json from 'url:./assets/data/world_low_geo.json';
+import worldMap from 'url:./assets/map/world_map.jpg';
 
 // Constants
-GLOBE_RADIUS = 1;
+GLOBE_RADIUS = 100;
 
 fetch(json).then(res => res.json()).then(countries => {
+  console.log(countries);
 
   // Scene
   const scene = new THREE.Scene();
@@ -39,8 +41,8 @@ fetch(json).then(res => res.json()).then(countries => {
   scene.add(graticuleObj);
   
   // Camera
-  const camera = new THREE.PerspectiveCamera(55, aspectRatio, 0.1, 100);
-  camera.position.z = 3;
+  const camera = new THREE.PerspectiveCamera(55, aspectRatio, 1, 2000);
+  camera.position.z = 200;
   scene.add(camera);
   
   const controls = new OrbitControls(camera, canvas);
@@ -51,6 +53,36 @@ fetch(json).then(res => res.json()).then(countries => {
   });
   renderer.setSize(sizes.width, sizes.height);
   renderer.render(scene, camera);
+
+  // Dots
+  const DOT_COUNT = 20000;
+  const dotsPositions = [];
+  const dotsIds = [];
+  const countryIds = [];
+  const vector = new THREE.Vector3();
+
+  for (let i = DOT_COUNT; i >= 0; i--) {
+    const phi = Math.acos(-1 + 2 * i / DOT_COUNT);
+    const theta = Math.sqrt(DOT_COUNT * Math.PI) * phi;
+
+    vector.setFromSphericalCoords(GLOBE_RADIUS, phi, theta);
+
+    const dotGeometry = new THREE.CircleGeometry(0.5, 5);
+    const dotMaterial = new THREE.MeshBasicMaterial( { color: '#fff' } );
+    const circle = new THREE.Mesh(dotGeometry, dotMaterial);
+    scene.add( circle );
+    dotGeometry.lookAt(vector);
+    dotGeometry.translate(vector.x, vector.y, vector.z);
+  }
+
+  new THREE.ImageLoader().load(worldMap, (image) => {
+		const canv = document.createElement( 'canvas' );
+		const context = canv.getContext( '2d' );
+    context.drawImage(image, 0, 0, 2000, 1000, 0, 0, 2000, 1000);
+    console.log(context);
+    const imageData = context.getImageData(0, 0, 2000, 1000);
+    console.log(imageData);
+  });
   
   const tick = () => {
     renderer.render(scene, camera);
