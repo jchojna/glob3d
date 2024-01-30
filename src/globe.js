d7670b9607247fe217039367effcca0600d3b39c
Jakub Chojna <jakub.chojna@gmail.com>
2024-01-30 22:34:15 +0100

    feat: update code in globe.js

--- src/globe.js
+++ src/globe.js
@@ -1,89 +1,126 @@
 import './styles.scss';
 import * as THREE from 'three';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
-import { GeoJsonGeometry } from 'three-geojson-geometry';
-import { geoGraticule10 } from 'd3-geo';
 import { polyfill, h3ToGeo, h3ToGeoBoundary } from 'h3-js';
 import { ConicPolygonGeometry } from 'three-conic-polygon-geometry';
+
+import * as _bfg from 'three/examples/jsm/utils/BufferGeometryUtils.js';
+const bfg = Object.assign({}, _bfg);
+const BufferGeometryUtils = bfg.BufferGeometryUtils || bfg;
+
 import json from 'url:./assets/data/world_low_geo.json';
-import worldMap from 'url:./assets/map/world_map.jpg';
 
 // Constants
-GLOBE_RADIUS = 100;
+const GLOBE_RADIUS = 100;
+const h3Indexes = [];
 
-fetch(json).then(res => res.json()).then(countries => {
-  console.log(countries);
+// Scene
+const scene = new THREE.Scene();
+const canvas = document.querySelector('canvas.webglobe');
 
-  // Scene
-  const scene = new THREE.Scene();
-  const canvas = document.querySelector('canvas.webglobe');
+fetch(json).then(res => res.json()).then(({ features }) => {
   
-  // Sizes
-  const sizes = {
-    width: window.innerWidth,
-    height: window.innerHeight
-  }
-  let aspectRatio = sizes.width / sizes.height;
-  
-  // Globe
-  const globeGeometry = new THREE.SphereBufferGeometry(GLOBE_RADIUS, 24, 24);
-  const defaultGlobeMaterial = new THREE.MeshBasicMaterial({
-    color: '#555',
-    transparent: true,
-    wireframe: true
-  });
-  const globe = new THREE.Mesh(globeGeometry, defaultGlobeMaterial);
-  scene.add(globe);
-  
-  // Graticules
-  const graticuleObj = new THREE.LineSegments(
-    new GeoJsonGeometry(geoGraticule10(), GLOBE_RADIUS, 2),
-    new THREE.LineBasicMaterial({ color: 'white', transparent: true, opacity: 0.8 })
+  const obj = new THREE.Mesh(
+    undefined,
+    new THREE.MeshNormalMaterial({ color: 'red' })
   );
-  scene.add(graticuleObj);
-  
-  // Camera
-  const camera = new THREE.PerspectiveCamera(55, aspectRatio, 1, 2000);
-  camera.position.z = 200;
-  scene.add(camera);
-  
-  const controls = new OrbitControls(camera, canvas);
-  controls.enableDamping = true;
-  
-  const renderer = new THREE.WebGLRenderer({
-    canvas
+  obj.__globeObjType = 'hexPolygon'; // Add object type
+
+  features.forEach(({ geometry }) => {
+    const { type, coordinates } = geometry;
+    // Get H3 indexes for all hexagons in Polygon or MultiPolygon
+    if (type === 'Polygon') {
+      polyfill(coordinates, 3, true).forEach(idx => h3Indexes.push(idx));
+    } else if (type === 'MultiPolygon') {
+      coordinates.forEach(coords => {
+        polyfill(coords, 3, true).forEach(idx => h3Indexes.push(idx));
+      });
+    } else {
+      console.warn(`Unsupported GeoJson geometry type (${type})`);
+    }
   });
-  renderer.setSize(sizes.width, sizes.height);
-  renderer.render(scene, camera);
 
-
-new THREE.ImageLoader().load(worldMap, (image) => {
-  const worldMapCanvas = document.getElementById('worldMapCanvas');
-  const context = worldMapCanvas.getContext('2d');
-  context.drawImage(image, 0, 0);
-  // const worldMapData = [...context.getImageData(0, 0, 2000, 1000).data];
-  const worldMapData = context.getImageData(0, 0, 2000, 1000).data;
-  // console.log(worldMapData);
-
-  // Dots
-  const dotsPositions = [];
-  const dotsIds = [];
-  const countryIds = [];
-  const vector = new THREE.Vector3();
-  const dotMaterial = new THREE.MeshNormalMaterial();
-  
-  for (let i = DOT_COUNT; i >= 0; i--) {
-    const phi = Math.acos(-1 + 2 * i / DOT_COUNT);
-    const theta = Math.sqrt(DOT_COUNT * Math.PI) * phi;
+  const hexBins = h3Indexes.map(h3Index => {
+    hexCenter = h3ToGeo(h3Index);
+    hexGeoJson = h3ToGeoBoundary(h3Index, true).reverse();
     
-    vector.setFromSphericalCoords(GLOBE_RADIUS, phi, theta);
+    return { h3Index, hexCenter, hexGeoJson };
+  });
 
-    const dotGeometry = new THREE.CircleGeometry(1, 6);
-    const circle = new THREE.Mesh(dotGeometry, dotMaterial);
-    scene.add( circle );
-    dotGeometry.lookAt(vector);
-    dotGeometry.translate(vector.x, vector.y, vector.z);
+  const margin = 0.2;
+  
+  obj.geometry = !hexBins.length
+    ? new THREE.BufferGeometry()
+    : BufferGeometryUtils.mergeBufferGeometries(hexBins.map(hex => {
+      // compute new geojson with relative margin
+      const relNum = (st, end, rat) => st - (st - end) * rat;
+      const [clat, clng] = hex.hexCenter;
+      const geoJson = margin === 0
+      ? hex.hexGeoJson
+      : hex.hexGeoJson
+      .map(([elng, elat]) => [[elng, clng], [elat, clat]]
+      .map(([st, end]) => relNum(st, end, margin)));
 
-    // const uv = pointToUV(dotGeometry.boundingSphere.center, this.position);
-  }
+      return new ConicPolygonGeometry(
+        [geoJson],
+        GLOBE_RADIUS,
+        GLOBE_RADIUS + 1,
+        false,
+        true,
+        false,
+        5
+      );
+    }));
+  scene.add(obj);
+}).catch(err => {
+  console.log("Error Reading data " + err);
+});;
+
+// Sizes
+const sizes = {
+  width: window.innerWidth,
+  height: window.innerHeight
+}
+let aspectRatio = sizes.width / sizes.height;
+
+// Globe
+const globeGeometry = new THREE.SphereBufferGeometry(GLOBE_RADIUS, 24, 24);
+const defaultGlobeMaterial = new THREE.MeshBasicMaterial({
+  color: '#555',
+  transparent: true,
+  opacity: 0.1,
+  wireframe: false
+});
+const globe = new THREE.Mesh(globeGeometry, defaultGlobeMaterial);
+scene.add(globe);
+
+// Camera
+const camera = new THREE.PerspectiveCamera(55, aspectRatio, 1, 2000);
+camera.position.z = 300;
+scene.add(camera);
+
+const controls = new OrbitControls(camera, canvas);
+controls.enableDamping = true;
+
+const renderer = new THREE.WebGLRenderer({
+  canvas,
+  antialias: true
+});
+renderer.setSize(sizes.width, sizes.height);
+renderer.render(scene, camera);
+
+const tick = () => {
+  renderer.render(scene, camera);
+  controls.update();
+  window.requestAnimationFrame(tick);
+}
+tick();
+
+window.addEventListener('resize', () => {
+  sizes.width = window.innerWidth;
+  sizes.height = window.innerHeight;
+  aspectRatio = sizes.width / sizes.height;
+  camera.aspect = aspectRatio;
+  camera.updateProjectionMatrix();
+  renderer.setSize(sizes.width, sizes.height);
 });
