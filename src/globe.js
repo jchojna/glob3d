f7d825071607b0debd2dd0b53ed5ebefc62cbc0f
Jakub Chojna <jakub.chojna@gmail.com>
2024-02-05 23:24:55 +0100

    refactor: improve asynchronous code

--- src/globe.js
+++ src/globe.js
@@ -1,36 +1,58 @@
 //// @ts-check
 
+////////////////////////////////////////////////////////////////////////////////
+// #region // Imports //////////////////////////////////////////////////////////
+
+import {
+  getH3Indexes,
+  getHexBin,
+  getRandomInt,
+  getNewGeoJson,
+  polar2Cartesian
+} from './globeHelpers';
+
 import './styles.scss';
 import * as THREE from 'three';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
-import { polyfill, geoToH3, h3ToGeo, h3ToGeoBoundary } from 'h3-js';
+import { geoToH3 } from 'h3-js';
 import { ConicPolygonGeometry } from 'three-conic-polygon-geometry';
 import * as dat from 'lil-gui';
-
 import * as _bfg from 'three/examples/jsm/utils/BufferGeometryUtils.js';
+
 const bfg = Object.assign({}, _bfg);
 const BufferGeometryUtils = bfg.BufferGeometryUtils || bfg;
 
 import json from 'url:./assets/data/world_low_geo.json';
 import matcapImage from 'url:./assets/matcaps/matcap_1.png';
-import { Color, Vector3 } from 'three';
 
-// Constants
+// #endregion //////////////////////////////////////////////////////////////////
+// #region // Constants ////////////////////////////////////////////////////////
+
 const GLOBE_RADIUS = 100;
-const HEX_RES = 3; 
+const HEX_RES = 3;
 const HEX_MARGIN = 0.2;
 const HEX_CURVATURE_RES = 5;
 
 const textureLoader = new THREE.TextureLoader();
 const matcapTexture = textureLoader.load(matcapImage);
-const baseApiQuery = 'https://api.gbif.org/v1/occurrence/search';
-const facets = `${baseApiQuery}?kingdomKey=1&phylumKey=44&classKey=212&facet=genusKey&genusKey.facetLimit=1200000&genusKey.facetOffset=0&limit=0`;
+
+// #endregion //////////////////////////////////////////////////////////////////
+// #region // URLs /////////////////////////////////////////////////////////////
+
+const baseGbifUrl = 'https://api.gbif.org/v1/occurrence/search';
+const facetsUrl = `${baseGbifUrl}?kingdomKey=1&phylumKey=44&classKey=212&facet=genusKey&genusKey.facetLimit=1200000&genusKey.facetOffset=0&limit=0`;
+
+const getGenusDataUrl = (baseUrl, genus, limit) => {
+  return `${baseUrl}?genusKey=${genus}&limit=${limit}`;
+}
 
 const getWikiUrl = (title, size = 300) => {
   return `https://en.wikipedia.org/w/api.php?action=query&titles=${title}&prop=pageimages&format=json&pithumbsize=${size}&origin=*`;
 }
 
-// Debug
+// #endregion //////////////////////////////////////////////////////////////////
+// #region // Debug ////////////////////////////////////////////////////////////
+
 const gui = new dat.GUI();
 
 // Scene
@@ -87,51 +109,6 @@ scene.add(camera);
 renderer.setSize(sizes.width, sizes.height);
 renderer.render(scene, camera);
 
-// Get H3 indexes for all hexagons in Polygon or MultiPolygon
-const getH3Indexes = (features) => {
-  const indexes = [];
-  features.forEach(({ geometry }) => {
-    const { type, coordinates } = geometry;
-    if (type === 'Polygon') {
-      polyfill(coordinates, HEX_RES, true).forEach(idx => indexes.push(idx));
-    } else if (type === 'MultiPolygon') {
-      coordinates.forEach(coords => {
-        polyfill(coords, HEX_RES, true).forEach(idx => indexes.push(idx));
-      });
-    } else {
-      console.warn(`Unsupported GeoJson geometry type (${type})`);
-    }
-  });
-  return indexes;
-}
-
-const getHexBin = (h3Index) => {
-  // Get center of a given hexagon - point as a [lat, lng] pair.
-  const center = h3ToGeo(h3Index);
-  // Get the vertices of a given hexagon as an array of [lat, lng] points.
-  const vertices = h3ToGeoBoundary(h3Index, true).reverse();
-  // Split geometries at the anti-meridian.
-  const centerLng = center[1];
-  vertices.forEach(d => {
-    const edgeLng = d[0];
-    if (Math.abs(centerLng - edgeLng) > 170) {
-      d[0] += (centerLng > edgeLng ? 360 : -360);
-    }
-  });    
-  return { h3Index, center, vertices };
-}
-
-// Compute new geojson with relative margin.
-const getNewGeoJson = (hex, margin) => {
-  const relNum = (st, end, rat) => st - (st - end) * rat;
-  const [clat, clng] = hex.center;
-  return margin === 0
-    ? hex.vertices
-    : hex.vertices
-      .map(([elng, elat]) => [[elng, clng], [elat, clat]]
-      .map(([st, end]) => relNum(st, end, HEX_MARGIN)));
-}
-
 const updateHexGlobeGeometry = (hexBins) => {
   return !hexBins.length
     ? new THREE.BufferGeometry()
@@ -161,20 +138,12 @@ const updateHexResultsGeometry = (bin) => {
   );
 }
 
-const polar2Cartesian = (lat, lng, relAltitude = 0) => {
-  const phi = (90 - lat) * Math.PI / 180;
-  const theta = (90 - lng) * Math.PI / 180;
-  const r = GLOBE_RADIUS * (1 + relAltitude) + 0.1;
-  return {
-    x: r * Math.sin(phi) * Math.cos(theta),
-    y: r * Math.cos(phi),
-    z: r * Math.sin(phi) * Math.sin(theta)
-  };
-}
+////////////////////////////////////////////////////////////////////////////////
+// #region // asynchronous code ////////////////////////////////////////////////
 
 // Create hexGlobe object.
 fetch(json).then(res => res.json()).then(({ features }) => {
-  const h3Indexes = getH3Indexes(features);
+  const h3Indexes = getH3Indexes(features, HEX_RES);
   const hexBins = h3Indexes.map(index => getHexBin(index));
   hexGlobe.geometry = updateHexGlobeGeometry(hexBins);
   scene.add(hexGlobe);
@@ -182,17 +151,7 @@ fetch(json).then(res => res.json()).then(({ features }) => {
   console.log("Error Reading data " + err);
 });
 
-const getRandomInt = (min, max) => {
-  min = Math.ceil(min);
-  max = Math.floor(max);
-  return Math.floor(Math.random() * (max - min)) + min;
-}
-
-////////////////////////////////////////////////////////////////////////////////
-// asynchronous code ///////////////////////////////////////////////////////////
-////////////////////////////////////////////////////////////////////////////////
-
-fetch(facets)
+fetch(facetsUrl)
   .then(res => res.json())
   .then(({ facets }) => {
     const genusData = facets[0].counts;
@@ -200,7 +159,7 @@ fetch(facets)
     return genusData[randomGenusIdx].name;
   })
   .then(genus => {
-    fetch(`${baseApiQuery}?kingdomKey=1&phylumKey=44&classKey=212&genusKey=${genus}&limit=1`)
+    fetch(getGenusDataUrl(baseGbifUrl, genus, 1))
       .then(res => res.json())
       .then(({ results }) => {
         fetch(getWikiUrl(results[0].genus))
@@ -216,48 +175,46 @@ fetch(facets)
       })
       .catch((error) => console.log(error));
 
-    const json = `${baseApiQuery}?limit=1000&genusKey=${genus}`;
+    fetch(getGenusDataUrl(baseGbifUrl, genus, 1000))
+      .then(res => res.json())
+      .then(({ results }) => {
+        resultsData = results
+          // Filter out results with undefined coordinates.
+          .filter(({ decimalLatitude, decimalLongitude }) => decimalLatitude && decimalLongitude)
+          .map(result => {
+            const { country, decimalLatitude, decimalLongitude } = result;
+            const h3Index = geoToH3(decimalLatitude, decimalLongitude, HEX_RES);
+            const hexBin = getHexBin(h3Index);
+            return {
+              country,
+              coordinates: [decimalLatitude, decimalLongitude],
+              ...hexBin,
+              occurrences: 1
+            }
+          })
+          .reduce((a, b) => {
+            const idx = a.findIndex(elem => elem.h3Index === b.h3Index)
+            if (idx >= 0) {
+              a[idx].occurrences++
+              return a
+            } else {
+              return [ ...a, b ]
+            }
+          }, []);
 
-    fetch(json).then(res => res.json()).then(({ results }) => {
-      resultsData = results
-        // Filter out results with undefined coordinates.
-        .filter(({ decimalLatitude, decimalLongitude }) => decimalLatitude && decimalLongitude)
-        .map(result => {
-          const { country, decimalLatitude, decimalLongitude } = result;
-          const h3Index = geoToH3(decimalLatitude, decimalLongitude, HEX_RES);
-          const hexBin = getHexBin(h3Index);
-          return {
-            country,
-            coordinates: [decimalLatitude, decimalLongitude],
-            ...hexBin,
-            occurrences: 1
-          }
-        })
-        .reduce((a, b) => {
-          const idx = a.findIndex(elem => elem.h3Index === b.h3Index)
-          if (idx >= 0) {
-            a[idx].occurrences++
-            return a
-          } else {
-            return [ ...a, b ]
-          }
-        }, []);
-
-      // Hexagonal Results
-      spotsMeshes = resultsData.map(bin => {
-        return new THREE.Mesh(
-          updateHexResultsGeometry(bin),
-          new THREE.MeshBasicMaterial({color: "red", side: THREE.DoubleSide})
-        );
+        // Hexagonal Results
+        spotsMeshes = resultsData.map(bin => {
+          return new THREE.Mesh(
+            updateHexResultsGeometry(bin),
+            new THREE.MeshBasicMaterial({color: "red", side: THREE.DoubleSide})
+          );
+        });
+        spotsMeshes.forEach(spot => scene.add(spot));
       });
-      spotsMeshes.forEach(spot => scene.add(spot));
-    });
-    return genus;
+      return genus;
   });
 
-////////////////////////////////////////////////////////////////////////////////
-// asynchronous code end ///////////////////////////////////////////////////////
-////////////////////////////////////////////////////////////////////////////////
+// #endregion //////////////////////////////////////////////////////////////////
 
 // Handle mouse
 const mouse = new THREE.Vector2();
@@ -270,7 +227,7 @@ window.addEventListener('mousemove', (e) => {
 });
 
 const getPixelPositionFromPolarCoords = (polarCoordinates) => {
-  const { x, y, z } = polar2Cartesian(polarCoordinates[0], polarCoordinates[1]);
+  const { x, y, z } = polar2Cartesian(polarCoordinates[0], polarCoordinates[1], GLOBE_RADIUS);
   const point = new THREE.Vector3(x, y, z).project(camera);
   return {
     x: (point.x + 1) / 2 * sizes.width,
@@ -291,6 +248,8 @@ window.addEventListener('click', function() {
 const raycaster = new THREE.Raycaster();
 const raycaster2 = new THREE.Raycaster();
 
+////////////////////////////////////////////////////////////////////////////////
+// #region // tick function ////////////////////////////////////////////////////
 const tick = () => {
   // const elapsedTime = clock.getElapsedTime();
 
@@ -323,7 +282,7 @@ const tick = () => {
     tooltip.style.transform = `translate(${pxPosition.x}px, ${pxPosition.y}px)`;
 
     // check collisions
-    const { x, y, z } = polar2Cartesian(polarCoordinates[0], polarCoordinates[1]);
+    const { x, y, z } = polar2Cartesian(polarCoordinates[0], polarCoordinates[1], GLOBE_RADIUS);
     const point = new THREE.Vector3(x, y, z).project(camera);
     raycaster2.setFromCamera(point, camera);
     const intersects = raycaster2.intersectObjects([globe, spotsMeshes[clickedHexIdx]]);
@@ -340,6 +299,7 @@ const tick = () => {
   controls.update();
   window.requestAnimationFrame(tick);
 }
+// #endregion //////////////////////////////////////////////////////////////////
 tick();
 
 window.addEventListener('resize', () => {
---
+++ src/globeHelpers.js
@@ -0,0 +1,63 @@
+import { polyfill, h3ToGeo, h3ToGeoBoundary } from 'h3-js';
+
+// Get H3 indexes for all hexagons in Polygon or MultiPolygon
+export const getH3Indexes = (features, resolution) => {
+  const indexes = [];
+  features.forEach(({ geometry }) => {
+    const { type, coordinates } = geometry;
+    if (type === 'Polygon') {
+      polyfill(coordinates, resolution, true).forEach(idx => indexes.push(idx));
+    } else if (type === 'MultiPolygon') {
+      coordinates.forEach(coords => {
+        polyfill(coords, resolution, true).forEach(idx => indexes.push(idx));
+      });
+    } else {
+      console.warn(`Unsupported GeoJson geometry type (${type})`);
+    }
+  });
+  return indexes;
+}
+
+export const getHexBin = (h3Index) => {
+  // Get center of a given hexagon - point as a [lat, lng] pair.
+  const center = h3ToGeo(h3Index);
+  // Get the vertices of a given hexagon as an array of [lat, lng] points.
+  const vertices = h3ToGeoBoundary(h3Index, true).reverse();
+  // Split geometries at the anti-meridian.
+  const centerLng = center[1];
+  vertices.forEach(d => {
+    const edgeLng = d[0];
+    if (Math.abs(centerLng - edgeLng) > 170) {
+      d[0] += (centerLng > edgeLng ? 360 : -360);
+    }
+  });    
+  return { h3Index, center, vertices };
+}
+
+export const getRandomInt = (min, max) => {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min)) + min;
+}
+
+// Compute new geojson with relative margin.
+export const getNewGeoJson = (hex, margin) => {
+  const relNum = (st, end, rat) => st - (st - end) * rat;
+  const [clat, clng] = hex.center;
+  return margin === 0
+    ? hex.vertices
+    : hex.vertices
+      .map(([elng, elat]) => [[elng, clng], [elat, clat]]
+      .map(([st, end]) => relNum(st, end, margin)));
+}
+
+export const polar2Cartesian = (lat, lng, globeRadius, relAltitude = 0) => {
+  const phi = (90 - lat) * Math.PI / 180;
+  const theta = (90 - lng) * Math.PI / 180;
+  const r = globeRadius * (1 + relAltitude) + 0.1;
+  return {
+    x: r * Math.sin(phi) * Math.cos(theta),
+    y: r * Math.cos(phi),
+    z: r * Math.sin(phi) * Math.sin(theta)
+  };
+}
