2e0504b8723d8f4344e53fd69f004366acf3a142
Jakub Chojna <jakub.chojna@gmail.com>
2024-02-06 22:20:01 +0100

    refactor: extract input data code

--- src/js/globe.js
+++ src/js/globe.js
@@ -9,7 +9,6 @@ import {
   getNewGeoJson,
   polar2Cartesian
 } from './globeHelpers';
-
 import '../styles.scss';
 import * as THREE from 'three';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
@@ -17,114 +16,177 @@ import { geoToH3 } from 'h3-js';
 import { ConicPolygonGeometry } from 'three-conic-polygon-geometry';
 import * as dat from 'lil-gui';
 import * as _bfg from 'three/examples/jsm/utils/BufferGeometryUtils.js';
-
-const bfg = Object.assign({}, _bfg);
-const BufferGeometryUtils = bfg.BufferGeometryUtils || bfg;
-
 import json from 'url:../assets/data/world_low_geo.json';
 import matcapImage from 'url:../assets/matcaps/matcap_1.png';
 
 // #endregion //////////////////////////////////////////////////////////////////
-// #region // Constants ////////////////////////////////////////////////////////
+// #region // Class ////////////////////////////////////////////////////////////
 
-const GLOBE_RADIUS = 100;
-const HEX_RES = 3;
-const HEX_MARGIN = 0.2;
-const HEX_CURVATURE_RES = 5;
+class World {
+  constructor(
+    globeRadius = 100,
+    hexRes = 3,
+    hexMargin = 0.2,
+    hexCurvatureRes = 5
+  ) {
+    this.bfg = Object.assign({}, _bfg);
+    this.BufferGeometryUtils = this.bfg.BufferGeometryUtils || this.bfg;
+    this.globeRadius = globeRadius;
+    this.hexRes = hexRes;
+    this.hexMargin = hexMargin;
+    this.hexCurvatureRes = hexCurvatureRes;
+    this.textureLoader = new THREE.TextureLoader();
+    this.matcapTexture = this.textureLoader.load(matcapImage);
+    // scene
+    this.scene = new THREE.Scene();
+    this.canvas = document.querySelector('canvas.webglobe');
+    this.tooltip = document.querySelector('.tooltip');
+    this.tooltipCountry = document.querySelector('.tooltip > .country');
+    this.tooltipOccurrences = document.querySelector('.tooltip > .occurrences');
+    // hexagonal globe
+    this.hexGlobeGeometry = undefined;
+    this.hexGlobeMaterial = new THREE.MeshMatcapMaterial();
+    this.hexGlobeMaterial.matcap = this.matcapTexture;
+    this.hexGlobe = new THREE.Mesh(this.hexGlobeGeometry, this.hexGlobeMaterial);
+    this.spotsMeshes = [];
+    this.aggregatedData = [];
+    // sizes
+    this.sizes = {
+      width: window.innerWidth,
+      height: window.innerHeight
+    }
+    this.aspectRatio = this.sizes.width / this.sizes.height;
+    // globe
+    this.solidGlobeGeometry = new THREE.SphereBufferGeometry(this.globeRadius, 32, 32);
+    this.solidGlobeMaterial = new THREE.MeshBasicMaterial({
+      color: '#555',
+      transparent: true,
+      opacity: 0.2,
+      wireframe: false
+    });
+    this.globe = new THREE.Mesh(this.solidGlobeGeometry, this.solidGlobeMaterial);
+    // camera
+    this.camera = new THREE.PerspectiveCamera(55, this.aspectRatio, 1, 2000);
+    this.camera.position.z = 150;
+    this.camera.position.y = 150;
+    // controls
+    this.controls = new OrbitControls(this.camera, this.canvas);
+    this.controls.enableDamping = true;
+    // renderer
+    this.renderer = new THREE.WebGLRenderer({
+      canvas: this.canvas,
+      antialias: true
+    });
+    this.scene.add(this.globe);
+    this.scene.add(this.camera);
+    this.renderer.setSize(this.sizes.width, this.sizes.height);
+    this.renderer.render(this.scene, this.camera);
+  }
 
-const textureLoader = new THREE.TextureLoader();
-const matcapTexture = textureLoader.load(matcapImage);
+  createHexGlobe() {
+    fetch(json)
+      .then(res => res.json())
+      .then(({ features }) => {
+        const h3Indexes = getH3Indexes(features, this.hexRes);
+        const hexBins = h3Indexes.map(index => getHexBin(index));
+        this.hexGlobe.geometry = this.updateHexGlobeGeometry(hexBins);
+        this.scene.add(this.hexGlobe);
+      })
+      .catch(err => {
+        console.log("Error Reading data " + err);
+      });
+  }
+  
+  updateHexGlobeGeometry(hexBins) {
+    return !hexBins.length
+      ? new THREE.BufferGeometry()
+      : this.BufferGeometryUtils.mergeBufferGeometries(hexBins.map(hex => {
+        const geoJson = getNewGeoJson(hex, this.hexMargin);
+        return new ConicPolygonGeometry(
+          [geoJson],
+          this.globeRadius,       // bottom height
+          this.globeRadius + 0.1, // top height
+          true,                   // closed bottom
+          true,                   // closed top
+          false,                  // include sides
+          this.hexCurvatureRes    // curvatureResolution
+        );
+      }));
+  }
+
+  updateHexResultsGeometry(bin) {
+    return new ConicPolygonGeometry(
+      [getNewGeoJson(bin, this.hexMargin)],
+      this.globeRadius + 0.1,
+      this.globeRadius + 0.1 + bin.occurrences / 3,
+      true,
+      true,
+      true,
+      this.hexCurvatureRes
+    );
+  }
+
+  aggregateData(results) {
+    return results
+      // Filter out results with undefined coordinates.
+      .filter(({ decimalLatitude, decimalLongitude }) => decimalLatitude && decimalLongitude)
+      .map(result => {
+        const { country, decimalLatitude, decimalLongitude } = result;
+        const h3Index = geoToH3(decimalLatitude, decimalLongitude, /* HEX_RES */ 3);
+        const hexBin = getHexBin(h3Index);
+        return {
+          country,
+          coordinates: [decimalLatitude, decimalLongitude],
+          ...hexBin,
+          occurrences: 1
+        }
+      })
+      .reduce((a, b) => {
+        const idx = a.findIndex(elem => elem.h3Index === b.h3Index)
+        if (idx >= 0) {
+          a[idx].occurrences++
+          return a
+        } else {
+          return [ ...a, b ]
+        }
+      }, []);
+  }
+
+  visualizeResult(aggregatedData) {
+    return aggregatedData.map(bin => {
+      return new THREE.Mesh(
+        this.updateHexResultsGeometry(bin),
+        new THREE.MeshBasicMaterial({color: "red", side: THREE.DoubleSide})
+      );
+    }).forEach(spot => this.scene.add(spot));
+  }
+
+  tick() {
+    this.renderer.render(this.scene, this.camera);
+    this.controls.update();
+    return window.requestAnimationFrame(() => this.tick());
+  }
+}
+
+
+
+
+
+
+
+// return false;
 
 // #endregion //////////////////////////////////////////////////////////////////
 // #region // Debug ////////////////////////////////////////////////////////////
 
-const gui = new dat.GUI();
+// const gui = new dat.GUI();
+// gui.addColor(solidGlobeMaterial, 'color');
+// gui.add(solidGlobeMaterial, 'opacity').min(0).max(1).step(0.01);
 
-// Scene
-const scene = new THREE.Scene();
-const canvas = document.querySelector('canvas.webglobe');
-const tooltip = document.querySelector('.tooltip');
-const tooltipCountry = document.querySelector('.tooltip > .country');
-const tooltipOccurrences = document.querySelector('.tooltip > .occurrences');
 
-// Hexagonal Globe
-const hexGlobeGeometry = undefined;
-const hexGlobeMaterial = new THREE.MeshMatcapMaterial();
-hexGlobeMaterial.matcap = matcapTexture;
-const hexGlobe = new THREE.Mesh(hexGlobeGeometry, hexGlobeMaterial);
-let spotsMeshes = [];
-let resultsData = [];
-
-// Sizes
-const sizes = {
-  width: window.innerWidth,
-  height: window.innerHeight
-}
-let aspectRatio = sizes.width / sizes.height;
-
-// Globe
-const solidGlobeGeometry = new THREE.SphereBufferGeometry(GLOBE_RADIUS, 32, 32);
-const solidGlobeMaterial = new THREE.MeshBasicMaterial({
-  color: '#555',
-  transparent: true,
-  opacity: 0.1,
-  wireframe: false
-});
-gui.addColor(solidGlobeMaterial, 'color');
-gui.add(solidGlobeMaterial, 'opacity').min(0).max(1).step(0.01);
-const globe = new THREE.Mesh(solidGlobeGeometry, solidGlobeMaterial);
-
-// Camera
-const camera = new THREE.PerspectiveCamera(55, aspectRatio, 1, 2000);
-camera.position.z = 150;
-camera.position.y = 150;
-
-// Controls
-const controls = new OrbitControls(camera, canvas);
-controls.enableDamping = true;
-
-// Renderer
-const renderer = new THREE.WebGLRenderer({
-  canvas,
-  antialias: true
-});
-
-scene.add(globe);
-scene.add(camera);
-renderer.setSize(sizes.width, sizes.height);
-renderer.render(scene, camera);
-
-const updateHexGlobeGeometry = (hexBins) => {
-  return !hexBins.length
-    ? new THREE.BufferGeometry()
-    : BufferGeometryUtils.mergeBufferGeometries(hexBins.map(hex => {
-      const geoJson = getNewGeoJson(hex, HEX_MARGIN);
-      return new ConicPolygonGeometry(
-        [geoJson],
-        GLOBE_RADIUS,       // bottom height
-        GLOBE_RADIUS + 0.1, // top height
-        true,               // closed bottom
-        true,               // closed top
-        false,              // include sides
-        HEX_CURVATURE_RES   // curvatureResolution
-      );
-    }));
-}
-
-const updateHexResultsGeometry = (bin) => {
-  return new ConicPolygonGeometry(
-    [getNewGeoJson(bin, HEX_MARGIN)],
-    GLOBE_RADIUS + 0.1,
-    GLOBE_RADIUS + 0.1 + bin.occurrences / 3,
-    true,
-    true,
-    true,
-    HEX_CURVATURE_RES
-  );
-}
 
 ////////////////////////////////////////////////////////////////////////////////
-// #region // asynchronous code ////////////////////////////////////////////////
+// #region // outside script ///////////////////////////////////////////////////
 
 const baseGbifUrl = 'https://api.gbif.org/v1/occurrence/search';
 const facetsUrl = `${baseGbifUrl}?kingdomKey=1&phylumKey=44&classKey=212&facet=genusKey&genusKey.facetLimit=1200000&genusKey.facetOffset=0&limit=0`;
@@ -136,16 +198,6 @@ const getGenusDataUrl = (baseUrl, genus, limit) => {
 const getWikiUrl = (title, size = 300) => {
   return `https://en.wikipedia.org/w/api.php?action=query&titles=${title}&prop=pageimages&format=json&pithumbsize=${size}&origin=*`;
 }
-
-// Create hexGlobe object.
-fetch(json).then(res => res.json()).then(({ features }) => {
-  const h3Indexes = getH3Indexes(features, HEX_RES);
-  const hexBins = h3Indexes.map(index => getHexBin(index));
-  hexGlobe.geometry = updateHexGlobeGeometry(hexBins);
-  scene.add(hexGlobe);
-}).catch(err => {
-  console.log("Error Reading data " + err);
-});
 
 fetch(facetsUrl)
   .then(res => res.json())
@@ -174,44 +226,44 @@ fetch(facetsUrl)
     fetch(getGenusDataUrl(baseGbifUrl, genus, 1000))
       .then(res => res.json())
       .then(({ results }) => {
-        resultsData = results
-          // Filter out results with undefined coordinates.
-          .filter(({ decimalLatitude, decimalLongitude }) => decimalLatitude && decimalLongitude)
-          .map(result => {
-            const { country, decimalLatitude, decimalLongitude } = result;
-            const h3Index = geoToH3(decimalLatitude, decimalLongitude, HEX_RES);
-            const hexBin = getHexBin(h3Index);
-            return {
-              country,
-              coordinates: [decimalLatitude, decimalLongitude],
-              ...hexBin,
-              occurrences: 1
-            }
-          })
-          .reduce((a, b) => {
-            const idx = a.findIndex(elem => elem.h3Index === b.h3Index)
-            if (idx >= 0) {
-              a[idx].occurrences++
-              return a
-            } else {
-              return [ ...a, b ]
-            }
-          }, []);
-
-        // Hexagonal Results
-        spotsMeshes = resultsData.map(bin => {
-          return new THREE.Mesh(
-            updateHexResultsGeometry(bin),
-            new THREE.MeshBasicMaterial({color: "red", side: THREE.DoubleSide})
-          );
-        });
-        spotsMeshes.forEach(spot => scene.add(spot));
+        const inputData = getInputData(results);
+        console.log(inputData)
+        const worldInstance = new World(120);
+        worldInstance.tick();
+        worldInstance.createHexGlobe();
+        const aggr = worldInstance.aggregateData(inputData);
+        worldInstance.visualizeResult(aggr);
       });
       return genus;
   });
 
-// #endregion //////////////////////////////////////////////////////////////////
 
+const getInputData = (dataArr) => {
+  return dataArr.map(({
+    continent,
+    country,
+    countryCode,
+    decimalLatitude,
+    decimalLongitude,
+    genus,
+    kingdom,
+    species,
+    eventDate
+  }) => ({
+    name: species,
+    decimalLatitude,
+    decimalLongitude,
+    date: new Date(eventDate),
+    country,
+    // continent,
+    // countryCode,
+    // genus,
+    // kingdom
+  }))
+}
+
+// #endregion //////////////////////////////////////////////////////////////////
+return false;
 // Handle mouse
 const mouse = new THREE.Vector2();
 let hoveredHexIdx = null;
@@ -250,26 +302,26 @@ const tick = () => {
   // const elapsedTime = clock.getElapsedTime();
 
   // Add Raycaster
-  if (spotsMeshes.length > 0) {
-    raycaster.setFromCamera(mouse, camera);
-    const intersects = raycaster.intersectObjects([globe, ...spotsMeshes]);
-    const intersectsMeshes = intersects.map(intersect => intersect.object);
-    hoveredHexIdx = null;
+  // if (spotsMeshes.length > 0) {
+  //   raycaster.setFromCamera(mouse, camera);
+  //   const intersects = raycaster.intersectObjects([globe, ...spotsMeshes]);
+  //   const intersectsMeshes = intersects.map(intersect => intersect.object);
+  //   hoveredHexIdx = null;
 
-    spotsMeshes.forEach((mesh, idx) => {
-      if (intersectsMeshes.includes(mesh)) {
-        const closestIntersect = intersects
-          .sort((a, b) => a.distance - b.distance)[0]
-          .object;
-        if (closestIntersect === mesh) {
-          mesh.material.color.set('blue');
-          hoveredHexIdx = idx;
-        }
-      } else {
-        if (idx !== clickedHexIdx) mesh.material.color.set('red');
-      }
-    });
-  }
+  //   spotsMeshes.forEach((mesh, idx) => {
+  //     if (intersectsMeshes.includes(mesh)) {
+  //       const closestIntersect = intersects
+  //         .sort((a, b) => a.distance - b.distance)[0]
+  //         .object;
+  //       if (closestIntersect === mesh) {
+  //         mesh.material.color.set('blue');
+  //         hoveredHexIdx = idx;
+  //       }
+  //     } else {
+  //       if (idx !== clickedHexIdx) mesh.material.color.set('red');
+  //     }
+  //   });
+  // }
   // Handle tooltip visibility of the clicked hex.
   if (clickedHexIdx !== null) {
     const clickedHexData = resultsData[clickedHexIdx]
