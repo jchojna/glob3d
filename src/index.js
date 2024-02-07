24fbd450f88768dd1a8825507dd8dd039aecd53c
Jakub Chojna <jakub.chojna@gmail.com>
2024-02-07 19:11:24 +0100

    refactor: move input data and class instance call to index.js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,77 @@
-export { default } from './js/globe';
+import WebGLobe from "./js/globe";
+import { getRandomInt } from './js/globeHelpers';
+
+const baseGbifUrl = 'https://api.gbif.org/v1/occurrence/search';
+const facetsUrl = `${baseGbifUrl}?kingdomKey=1&phylumKey=44&classKey=212&facet=genusKey&genusKey.facetLimit=1200000&genusKey.facetOffset=0&limit=0`;
+
+const getGenusDataUrl = (baseUrl, genus, limit) => {
+  return `${baseUrl}?genusKey=${genus}&limit=${limit}`;
+}
+
+const getWikiUrl = (title, size = 300) => {
+  return `https://en.wikipedia.org/w/api.php?action=query&titles=${title}&prop=pageimages&format=json&pithumbsize=${size}&origin=*`;
+}
+
+fetch(facetsUrl)
+  .then(res => res.json())
+  .then(({ facets }) => {
+    const genusData = facets[0].counts;
+    const randomGenusIdx = getRandomInt(0, genusData.length);
+    return genusData[randomGenusIdx].name;
+  })
+  .then(genus => {
+    fetch(getGenusDataUrl(baseGbifUrl, genus, 1))
+      .then(res => res.json())
+      .then(({ results }) => {
+        fetch(getWikiUrl(results[0].genus))
+          .then(res => res.json())
+          .then(({ query }) => {
+            const { pages } = query;
+            Object.values(pages).forEach(value => {
+              const { source } = value.thumbnail;
+              source ? console.log(source) : false;
+            });
+          })
+          .catch((error) => console.log(error));
+      })
+      .catch((error) => console.log(error));
+
+    fetch(getGenusDataUrl(baseGbifUrl, genus, 1000))
+      .then(res => res.json())
+      .then(({ results }) => {
+        const inputData = getInputData(results);
+        // console.log(inputData);
+        
+        // refactor - one call?
+        const worldInstance = new WebGLobe(120);
+        worldInstance.initialize(inputData);
+      });
+      return genus;
+  });
+
+
+const getInputData = (dataArr) => {
+  return dataArr.map(({
+    continent,
+    country,
+    countryCode,
+    decimalLatitude,
+    decimalLongitude,
+    genus,
+    kingdom,
+    species,
+    eventDate
+  }) => {
+
+    return decimalLatitude && decimalLatitude ? {
+      name: species,
+      coordinates: [decimalLatitude, decimalLongitude],
+      date: new Date(eventDate),
+      country,
+      // continent,
+      // countryCode,
+      // genus,
+      // kingdom
+    } : null;
+  }).filter(e => e);
+}
--- src/js/globe.js
+++ src/js/globe.js
@@ -1,11 +1,6 @@
-//// @ts-check
-
-// #region // Imports //////////////////////////////////////////////////////////
-
 import {
   getH3Indexes,
   getHexBin,
-  getRandomInt,
   getNewGeoJson,
   polar2Cartesian
 } from './globeHelpers';
@@ -19,10 +14,7 @@ import * as _bfg from 'three/examples/jsm/utils/BufferGeometryUtils.js';
 import json from 'url:../assets/data/world_low_geo.json';
 import matcapImage from 'url:../assets/matcaps/matcap_1.png';
 
-// #endregion //////////////////////////////////////////////////////////////////
-// #region // Class ////////////////////////////////////////////////////////////
-
-class World {
+export default class WebGLobe {
   constructor(
     globeRadius = 100,
     hexRes = 3,
@@ -256,89 +248,6 @@ class World {
   }
 }
 
-// #endregion //////////////////////////////////////////////////////////////////
-// #region // Debug ////////////////////////////////////////////////////////////
-
 // const gui = new dat.GUI();
 // gui.addColor(solidGlobeMaterial, 'color');
 // gui.add(solidGlobeMaterial, 'opacity').min(0).max(1).step(0.01);
-
-// #endregion //////////////////////////////////////////////////////////////////
-// #region // Outside script ///////////////////////////////////////////////////
-
-const baseGbifUrl = 'https://api.gbif.org/v1/occurrence/search';
-const facetsUrl = `${baseGbifUrl}?kingdomKey=1&phylumKey=44&classKey=212&facet=genusKey&genusKey.facetLimit=1200000&genusKey.facetOffset=0&limit=0`;
-
-const getGenusDataUrl = (baseUrl, genus, limit) => {
-  return `${baseUrl}?genusKey=${genus}&limit=${limit}`;
-}
-
-const getWikiUrl = (title, size = 300) => {
-  return `https://en.wikipedia.org/w/api.php?action=query&titles=${title}&prop=pageimages&format=json&pithumbsize=${size}&origin=*`;
-}
-
-fetch(facetsUrl)
-  .then(res => res.json())
-  .then(({ facets }) => {
-    const genusData = facets[0].counts;
-    const randomGenusIdx = getRandomInt(0, genusData.length);
-    return genusData[randomGenusIdx].name;
-  })
-  .then(genus => {
-    fetch(getGenusDataUrl(baseGbifUrl, genus, 1))
-      .then(res => res.json())
-      .then(({ results }) => {
-        fetch(getWikiUrl(results[0].genus))
-          .then(res => res.json())
-          .then(({ query }) => {
-            const { pages } = query;
-            Object.values(pages).forEach(value => {
-              const { source } = value.thumbnail;
-              source ? console.log(source) : false;
-            });
-          })
-          .catch((error) => console.log(error));
-      })
-      .catch((error) => console.log(error));
-
-    fetch(getGenusDataUrl(baseGbifUrl, genus, 1000))
-      .then(res => res.json())
-      .then(({ results }) => {
-        const inputData = getInputData(results);
-        // console.log(inputData);
-        
-        // refactor - one call?
-        const worldInstance = new World(120);
-        worldInstance.initialize(inputData);
-      });
-      return genus;
-  });
-
-
-const getInputData = (dataArr) => {
-  return dataArr.map(({
-    continent,
-    country,
-    countryCode,
-    decimalLatitude,
-    decimalLongitude,
-    genus,
-    kingdom,
-    species,
-    eventDate
-  }) => {
-
-    return decimalLatitude && decimalLatitude ? {
-      name: species,
-      coordinates: [decimalLatitude, decimalLongitude],
-      date: new Date(eventDate),
-      country,
-      // continent,
-      // countryCode,
-      // genus,
-      // kingdom
-    } : null;
-  }).filter(e => e);
-}
-
-// #endregion //////////////////////////////////////////////////////////////////
