ecedfa3665d7a4460797291b679dd35b9aea6df9
Jakub Chojna <jakub.chojna@gmail.com>
2024-04-22 20:57:36 +0200

    test: add tests for getXYZCoordinates()

--- src/lib/helpers.test.js
+++ src/lib/helpers.test.js
@@ -1,6 +1,6 @@
 import { describe, expect, it } from 'vitest';
 
-import { getTooltipScale } from './helpers';
+import { getTooltipScale, getXYZCoordinates } from './helpers';
 
 describe('getTooltipScale', () => {
   it('returns correct value when distance is equal to minDistance', () => {
@@ -39,5 +39,19 @@ describe('getTooltipScale', () => {
 
   it('expects error when all arguments values are equal', () => {
     expect(() => getTooltipScale(1, 1, 1)).toThrowError();
+  });
+});
+
+describe('getXYZCoordinates', () => {
+  it('', () => {
+    const coordinates = getXYZCoordinates(0, 0, 0, 0);
+    expect(coordinates).toEqual({ x: 0, y: 0, z: 0 });
+  });
+
+  it('', () => {
+    const coordinates = getXYZCoordinates(20, 50, 10, 0);
+    expect(coordinates.x).toBeCloseTo(7.2, 2);
+    expect(coordinates.y).toBeCloseTo(3.42, 2);
+    expect(coordinates.z).toBeCloseTo(6.04, 2);
   });
 });
