708e32b3e970903f93c2e2d7d04192f82d951f2e
Jakub Chojna <jakub.chojna@appsilon.com>
2024-04-22 22:40:42 +0200

    Revert "feat: make the least number of fields and methods public"

--- src/lib/barGlobe.ts
+++ src/lib/barGlobe.ts
@@ -9,26 +9,26 @@ import { getHexBin, getNewGeoJson, getXYZCoordinates } from './helpers';
 import Tooltip from './tooltip';
 
 export default class BarGlob3d extends Glob3d {
-  #aggregatedData: HexData[];
-  #barColor: string;
-  #barOpacity: number;
-  #barActiveColor: string;
-  #barActiveOpacity: number;
-  #clickedHexId: string | null;
-  #clickedHexObject: HexResult | null;
-  #hexMaxValue: number;
-  #hexResults: HexResult[];
-  #hexResultsGroup: THREE.Object3D | THREE.Group;
-  #highestBar: number;
-  #hoveredHexId: string | null;
-  #hoveredHexObject: HexResult | null;
-  #raycaster: THREE.Raycaster;
-  #tooltips: TooltipProperties[] | null;
-  #tooltipActiveBackgroundColor: string;
-  #tooltipActiveTextColor: string;
-  #tooltipsContainer: HTMLElement | null;
-  #tooltipsLimit: number | null;
-  #tooltipValueSuffix: string;
+  aggregatedData: HexData[];
+  barColor: string;
+  barOpacity: number;
+  barActiveColor: string;
+  barActiveOpacity: number;
+  clickedHexId: string | null;
+  clickedHexObject: HexResult | null;
+  hexMaxValue: number;
+  hexResults: HexResult[];
+  hexResultsGroup: THREE.Object3D | THREE.Group;
+  highestBar: number;
+  hoveredHexId: string | null;
+  hoveredHexObject: HexResult | null;
+  raycaster: THREE.Raycaster;
+  tooltips: TooltipProperties[] | null;
+  tooltipActiveBackgroundColor: string;
+  tooltipActiveTextColor: string;
+  tooltipsContainer: HTMLElement | null;
+  tooltipsLimit: number | null;
+  tooltipValueSuffix: string;
 
   constructor(
     root: HTMLElement,
@@ -60,38 +60,36 @@ export default class BarGlob3d extends Glob3d {
       hexRes,
     });
 
-    this.#aggregatedData = [];
-    this.#barColor = barColor;
-    this.#barOpacity = barOpacity;
-    this.#barActiveColor = barActiveColor;
-    this.#barActiveOpacity = barActiveOpacity;
-    this.#clickedHexId = null;
-    this.#clickedHexObject = null;
-    this.#hexMaxValue = 1; // neutral value in the implementation
-    this.#hexResults = [];
-    this.#hexResultsGroup = new THREE.Group();
-    this.#highestBar = highestBar;
-    this.#hoveredHexId = null;
-    this.#hoveredHexObject = null;
-    this.#raycaster = new THREE.Raycaster();
-    this.#tooltips = null;
-    this.#tooltipActiveBackgroundColor = tooltipActiveBackgroundColor;
-    this.#tooltipActiveTextColor = tooltipActiveTextColor;
-    this.#tooltipsContainer = null;
-    this.#tooltipsLimit = tooltipsLimit;
-    this.#tooltipValueSuffix = tooltipValueSuffix;
+    this.aggregatedData = [];
+    this.barColor = barColor;
+    this.barOpacity = barOpacity;
+    this.barActiveColor = barActiveColor;
+    this.barActiveOpacity = barActiveOpacity;
+    this.clickedHexId = null;
+    this.clickedHexObject = null;
+    this.hexMaxValue = 1; // neutral value in the implementation
+    this.hexResults = [];
+    this.hexResultsGroup = new THREE.Group();
+    this.highestBar = highestBar;
+    this.hoveredHexId = null;
+    this.hoveredHexObject = null;
+    this.raycaster = new THREE.Raycaster();
+    this.tooltips = null;
+    this.tooltipActiveBackgroundColor = tooltipActiveBackgroundColor;
+    this.tooltipActiveTextColor = tooltipActiveTextColor;
+    this.tooltipsContainer = null;
+    this.tooltipsLimit = tooltipsLimit;
+    this.tooltipValueSuffix = tooltipValueSuffix;
 
-    this.#barTick();
-    this.#aggregatedData = this.#aggregateData(data);
-    this.#hexMaxValue = Math.max(
-      ...this.#aggregatedData.map((obj) => obj.value)
-    );
-    this.#hexResults = this.#visualizeResult(this.#aggregatedData);
-    this.#createTooltips();
-    this.#registerClickEvent();
+    this.barTick();
+    this.aggregatedData = this.aggregateData(data);
+    this.hexMaxValue = Math.max(...this.aggregatedData.map((obj) => obj.value));
+    this.hexResults = this.visualizeResult(this.aggregatedData);
+    this.createTooltips();
+    this.registerClickEvent();
   }
 
-  #preProcessData(data: GlobeData[]) {
+  preProcessData(data: GlobeData[]) {
     return data.map(({ city, country, coordinates, value }): HexData => {
       const h3Index = latLngToCell(
         coordinates.lat,
@@ -110,27 +108,24 @@ export default class BarGlob3d extends Glob3d {
     });
   }
 
-  #aggregateData(data: GlobeData[]) {
-    return this.#preProcessData(data).reduce(
-      (acc: HexData[], curr: HexData) => {
-        const idx = acc.findIndex(
-          (elem: { h3Index: string }) => elem.h3Index === curr.h3Index
-        );
-        if (idx >= 0) {
-          if (curr.city) acc[idx].city += `, ${curr.city}`;
-          acc[idx].value += curr.value;
-          return acc;
-        } else {
-          return [...acc, curr];
-        }
-      },
-      []
-    );
+  aggregateData(data: GlobeData[]) {
+    return this.preProcessData(data).reduce((acc: HexData[], curr: HexData) => {
+      const idx = acc.findIndex(
+        (elem: { h3Index: string }) => elem.h3Index === curr.h3Index
+      );
+      if (idx >= 0) {
+        if (curr.city) acc[idx].city += `, ${curr.city}`;
+        acc[idx].value += curr.value;
+        return acc;
+      } else {
+        return [...acc, curr];
+      }
+    }, []);
   }
 
-  #renderHexResultsGeometry(hex: HexData) {
-    if (!this.#hexMaxValue) return;
-    const offset = this.#getOffsetFromCenter(hex.value);
+  renderHexResultsGeometry(hex: HexData) {
+    if (!this.hexMaxValue) return;
+    const offset = this.getOffsetFromCenter(hex.value);
     return new ConicPolygonGeometry(
       [getNewGeoJson(hex, this.hexPadding)],
       this.globeRadius,
@@ -142,62 +137,60 @@ export default class BarGlob3d extends Glob3d {
     );
   }
 
-  #visualizeResult(aggregatedData: HexData[]) {
+  visualizeResult(aggregatedData: HexData[]) {
     const hexResults = aggregatedData.map((hexData: HexData) => {
       return new THREE.Mesh(
-        this.#renderHexResultsGeometry(hexData),
+        this.renderHexResultsGeometry(hexData),
         new THREE.MeshBasicMaterial({
-          color: this.#barColor,
-          opacity: this.#barOpacity,
+          color: this.barColor,
+          opacity: this.barOpacity,
           side: THREE.DoubleSide,
           transparent: true,
         })
       );
     });
-    this.#aggregatedData = this.#aggregatedData.map((hexData: HexData, i) => ({
+    this.aggregatedData = this.aggregatedData.map((hexData: HexData, i) => ({
       ...hexData,
       id: hexResults[i].uuid,
     }));
-    hexResults.forEach((hex: HexResult) => this.#hexResultsGroup.add(hex));
-    if (typeof this.#tooltipsLimit != 'number')
-      this.#tooltipsLimit = hexResults.length;
-    this.scene.add(this.#hexResultsGroup);
+    hexResults.forEach((hex: HexResult) => this.hexResultsGroup.add(hex));
+    if (typeof this.tooltipsLimit != 'number')
+      this.tooltipsLimit = hexResults.length;
+    this.scene.add(this.hexResultsGroup);
     return hexResults;
   }
 
-  #getOffsetFromCenter(value: number): number {
+  getOffsetFromCenter(value: number): number {
     return (
       this.globeRadius +
-      (value / this.#hexMaxValue) * this.globeRadius * 2 * this.#highestBar
+      (value / this.hexMaxValue) * this.globeRadius * 2 * this.highestBar
     );
   }
 
   // update tooltips reference points distances to the camera
-  #updateCameraForTooltips() {
-    if (!this.#tooltips) return;
-    this.#tooltips.forEach((tooltip) =>
-      tooltip.handleCameraUpdate(this.camera)
-    );
+  updateCameraForTooltips() {
+    if (!this.tooltips) return;
+    this.tooltips.forEach((tooltip) => tooltip.handleCameraUpdate(this.camera));
   }
 
-  #updateTooltipsOrder() {
-    if (!this.#tooltips) return;
-    const sortedTooltips = this.#tooltips.sort(
+  updateTooltipsOrder() {
+    if (!this.tooltips) return;
+    const sortedTooltips = this.tooltips.sort(
       (a, b) => a.distance - b.distance
     );
     const distances = sortedTooltips
       .map((tooltip) => tooltip.distance)
-      .slice(0, this.#tooltipsLimit || sortedTooltips.length);
+      .slice(0, this.tooltipsLimit || sortedTooltips.length);
 
     sortedTooltips.forEach((tooltip, i) => {
       if (
-        tooltip.id === this.#hoveredHexId ||
-        tooltip.id === this.#clickedHexId
+        tooltip.id === this.hoveredHexId ||
+        tooltip.id === this.clickedHexId
       ) {
         tooltip.show(true);
       } else if (
-        typeof this.#tooltipsLimit === 'number' &&
-        i < this.#tooltipsLimit
+        typeof this.tooltipsLimit === 'number' &&
+        i < this.tooltipsLimit
       ) {
         tooltip.updateOrder(i, Math.min(...distances), Math.max(...distances));
         tooltip.show();
@@ -207,39 +200,39 @@ export default class BarGlob3d extends Glob3d {
     });
   }
 
-  #getValueRank(value: number, values: number[]): number {
+  getValueRank(value: number, values: number[]): number {
     return values.filter((val: number) => val > value).length + 1;
   }
 
   // TODO: refactor the method
-  #createTooltips() {
-    this.#tooltips = this.#aggregatedData.map(
+  createTooltips() {
+    this.tooltips = this.aggregatedData.map(
       ({ id, center, country, city, value }: HexData) => {
-        const offset = this.#getOffsetFromCenter(value);
-        const valueRank = this.#getValueRank(
+        const offset = this.getOffsetFromCenter(value);
+        const valueRank = this.getValueRank(
           value,
-          this.#aggregatedData.map((hex) => hex.value)
+          this.aggregatedData.map((hex) => hex.value)
         );
         const coordinates = getXYZCoordinates(center[0], center[1], offset);
         return new Tooltip(
           id,
           coordinates,
           this.sizes,
-          this.#tooltipsLimit || this.#aggregateData.length,
+          this.tooltipsLimit || this.aggregateData.length,
           value,
           {
             city,
             country,
             mask: this.globe,
-            tooltipActiveBackgroundColor: this.#tooltipActiveBackgroundColor,
-            tooltipActiveTextColor: this.#tooltipActiveTextColor,
-            tooltipValueSuffix: this.#tooltipValueSuffix,
+            tooltipActiveBackgroundColor: this.tooltipActiveBackgroundColor,
+            tooltipActiveTextColor: this.tooltipActiveTextColor,
+            tooltipValueSuffix: this.tooltipValueSuffix,
             valueRank,
           }
         );
       }
     );
-    const tooltipsElements = this.#tooltips.map((tooltip) => tooltip.element);
+    const tooltipsElements = this.tooltips.map((tooltip) => tooltip.element);
     const tooltips = document.createElement('div');
     // TODO: use styled-components
     tooltips.style.height = '100%';
@@ -250,32 +243,32 @@ export default class BarGlob3d extends Glob3d {
     tooltips.style.top = '0';
     tooltips.style.pointerEvents = 'none';
     tooltips.append(...tooltipsElements);
-    this.#tooltipsContainer = tooltips;
+    this.tooltipsContainer = tooltips;
     this.root.style.position = 'relative';
     this.root.appendChild(tooltips);
   }
 
-  #highlightHex(object: HexResult | null) {
+  highlightHex(object: HexResult | null) {
     if (!object) return;
-    object.material.color.set(this.#barActiveColor);
-    object.material.opacity = this.#barActiveOpacity;
+    object.material.color.set(this.barActiveColor);
+    object.material.opacity = this.barActiveOpacity;
   }
 
-  #unhighlightHex(object: HexResult | null) {
+  unhighlightHex(object: HexResult | null) {
     if (!object) return;
-    object.material.color.set(this.#barColor);
-    object.material.opacity = this.#barOpacity;
+    object.material.color.set(this.barColor);
+    object.material.opacity = this.barOpacity;
   }
 
-  #barTick(): number {
-    if (this.#hexResults.length > 0) {
-      this.#raycaster.setFromCamera(this.mouse, this.camera);
-      this.#updateCameraForTooltips();
-      this.#updateTooltipsOrder();
+  barTick(): number {
+    if (this.hexResults.length > 0) {
+      this.raycaster.setFromCamera(this.mouse, this.camera);
+      this.updateCameraForTooltips();
+      this.updateTooltipsOrder();
 
-      const intersects = this.#raycaster.intersectObjects([
+      const intersects = this.raycaster.intersectObjects([
         this.globe,
-        ...this.#hexResults,
+        ...this.hexResults,
       ]);
       const hoveredHexObject =
         intersects.length > 0 &&
@@ -288,51 +281,51 @@ export default class BarGlob3d extends Glob3d {
         const hoveredHexId = hoveredHexObject.uuid;
 
         // on mouse over
-        if (this.#hoveredHexId !== hoveredHexId) {
-          this.#hoveredHexObject &&
-            this.#hoveredHexObject !== this.#clickedHexObject &&
-            this.#unhighlightHex(this.#hoveredHexObject);
-          this.#highlightHex(hoveredHexObject);
+        if (this.hoveredHexId !== hoveredHexId) {
+          this.hoveredHexObject &&
+            this.hoveredHexObject !== this.clickedHexObject &&
+            this.unhighlightHex(this.hoveredHexObject);
+          this.highlightHex(hoveredHexObject);
 
-          this.#hoveredHexObject = hoveredHexObject;
-          this.#hoveredHexId = hoveredHexId;
+          this.hoveredHexObject = hoveredHexObject;
+          this.hoveredHexId = hoveredHexId;
         }
       } else {
-        this.#hoveredHexObject &&
-          this.#hoveredHexObject !== this.#clickedHexObject &&
-          this.#unhighlightHex(this.#hoveredHexObject);
-        this.#hoveredHexObject = null;
-        this.#hoveredHexId = null;
+        this.hoveredHexObject &&
+          this.hoveredHexObject !== this.clickedHexObject &&
+          this.unhighlightHex(this.hoveredHexObject);
+        this.hoveredHexObject = null;
+        this.hoveredHexId = null;
       }
     }
 
-    return window.requestAnimationFrame(() => this.#barTick());
+    return window.requestAnimationFrame(() => this.barTick());
   }
 
-  #registerClickEvent() {
+  registerClickEvent() {
     window.addEventListener('click', () => {
-      if (this.#hoveredHexId) {
-        this.#clickedHexObject && this.#unhighlightHex(this.#clickedHexObject);
-        this.#clickedHexObject = this.#hoveredHexObject;
-        this.#clickedHexId = this.#hoveredHexId;
-        this.#highlightHex(this.#clickedHexObject);
+      if (this.hoveredHexId) {
+        this.clickedHexObject && this.unhighlightHex(this.clickedHexObject);
+        this.clickedHexObject = this.hoveredHexObject;
+        this.clickedHexId = this.hoveredHexId;
+        this.highlightHex(this.clickedHexObject);
       } else {
-        this.#unhighlightHex(this.#clickedHexObject);
-        this.#clickedHexObject = null;
-        this.#clickedHexId = null;
+        this.unhighlightHex(this.clickedHexObject);
+        this.clickedHexObject = null;
+        this.clickedHexId = null;
       }
     });
   }
 
   update(data: GlobeData[]) {
     // remove old elements
-    this.#hexResultsGroup.clear();
-    if (this.#tooltipsContainer) this.root.removeChild(this.#tooltipsContainer);
+    this.hexResultsGroup.clear();
+    if (this.tooltipsContainer) this.root.removeChild(this.tooltipsContainer);
     // create new elements
-    this.#aggregatedData = this.#aggregateData(
+    this.aggregatedData = this.aggregateData(
       data.slice(0, Math.round(Math.random() * 100))
     );
-    this.#hexResults = this.#visualizeResult(this.#aggregatedData);
-    this.#createTooltips();
+    this.hexResults = this.visualizeResult(this.aggregatedData);
+    this.createTooltips();
   }
 }
