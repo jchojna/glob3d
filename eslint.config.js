62796fc8bc0dae1633e0e3bdf23df00874fb4197
Jakub Chojna <jakub.chojna@gmail.com>
2024-04-25 20:18:55 +0200

    chore: add eslint

---
+++ eslint.config.js
@@ -0,0 +1,9 @@
+import pluginJs from '@eslint/js';
+import globals from 'globals';
+import tseslint from 'typescript-eslint';
+
+export default [
+  { languageOptions: { globals: globals.browser } },
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+];
